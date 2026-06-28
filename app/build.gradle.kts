2e0c97b0f55aa194d3b480df4a49402b3563b089
alfisjahri <alfisjahri@gmail.com>
2026-06-29 06:24:19 +0800

    fix 2 tombol "DARI GALERI" dan "KETIK LINK", ganti nama apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.smpn1damai.exambro"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resConfigs("id", "en")
